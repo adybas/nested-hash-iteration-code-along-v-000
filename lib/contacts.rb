00d6c9c3cd60a95bbb2f3e5799dc668709f2af31
Anna Dybas <a.m.dybas@gmail.com>
2019-05-25 16:27:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,6 +19,7 @@ def remove_strawberry(contacts)
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
         value.each do |flavor|
+          if flavor == "strawberry"
           flavor.delete("strawberry")
         end
       end
